0d13f9342bbe654d4db1eec71bd884e09d1371c0
Melissa Benard <benardmeli@yahoo.fr>
2023-12-31 18:45:57 +0100

    Use Bootstrap 5for the style

--- js_server/manageQuestionnaire.js
+++ js_server/manageQuestionnaire.js
@@ -1,4 +1,3 @@
-var mysql = require("mysql");
 var db = require("./db.js");
 var managePage = require("./managePage.js"); 
 
@@ -12,7 +11,15 @@ var manageQuestionnaire = {
                 console.log('Les questions sont: ', results);
                 var txt = ""; 
                 for (var question of results) {
-                    txt += question['idQuestions'] + " : " + question['descriptionQuestion'] + "<br/>"; 
+                    txt += '<div class="feature col border border-rounded p-2 gx-1">';
+                    txt += '<h3 class="">' + question['idQuestions'] + " : " + question['descriptionQuestion'] + '</h3>'
+                    txt += '<ul class="list-unstyled">'
+                    txt += '<li>Réponse 1 : ' + question['reponseA'] + '</li>'
+                    txt += '<li>Réponse 2 : ' + question['reponseB'] + '</li>'
+                    txt += '<li>Réponse 3 : ' + question['reponseC'] + '</li>'
+                    txt += '<li>Réponse 4 : ' + question['reponseD'] + '</li>'
+                    txt += '</ul>'
+                    txt += '</div>'
                 }
                 
                 managePage.objectToSupplant.Questions = txt;
